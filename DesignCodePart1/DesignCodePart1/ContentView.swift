9f9cbb3721da60cff3b53d21e6150b982c6b8965
zziro95 <opers7878@gmail.com>
2021-08-16 23:07:02 +0900

    initial commit

---
+++ DesignCodePart1/DesignCodePart1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodePart1
+//
+//  Created by Jinho Choi on 2021/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCodePart1/DesignCodePart1/DesignCodePart1App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodePart1App.swift
+//  DesignCodePart1
+//
+//  Created by Jinho Choi on 2021/08/16.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodePart1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
