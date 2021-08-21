e50330aad9de02e624ad8339e260bdde5ad493be
zziro95 <opers7878@gmail.com>
2021-08-21 23:55:41 +0900

    feat: part1-5 강의 듣기 완료

--- DesignCodePart1/DesignCodePart1/ContentView.swift
+++ DesignCodePart1/DesignCodePart1/ContentView.swift
@@ -16,6 +16,7 @@ import SwiftUI
 struct ContentView: View {
 
     @State var show = false
+    @State var viewState = CGSize.zero
 
     var body: some View {
         ZStack {
@@ -28,6 +29,7 @@ struct ContentView: View {
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -400 : -40)
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.90)
                 .rotationEffect(.degrees(show ? 0 : 10))
                 .rotation3DEffect(.degrees(10), axis: (x: 10.0, y: 0.0, z: 0.0))
@@ -39,6 +41,7 @@ struct ContentView: View {
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -200 : -20)
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.95)
                 .rotationEffect(.degrees(show ? 0 : 5))
                 .rotation3DEffect(.degrees(5), axis: (x: 10.0, y: 0.0, z: 0.0))
@@ -46,10 +49,22 @@ struct ContentView: View {
                 .animation(.easeIn(duration: 0.3))
 
             CardView()
+                .offset(x: viewState.width, y: viewState.height)
                 .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
                 .onTapGesture {
                     self.show.toggle()
                 }
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.viewState = value.translation
+                        self.show = true
+                    }
+                    .onEnded { value in
+                        self.viewState = .zero
+                        self.show = false
+                    }
+                )
 
             BottomCardView()
                 .blur(radius: show ? 20 : 0)
