f44c81df1d4c6f17363eedc68c5628c5c601c738
zziro95 <opers7878@gmail.com>
2021-08-25 23:48:34 +0900

    feat: session2 강의 듣기 완료, 정리 초본

--- Combine and Data.md
+++ Combine and Data.md
@@ -19,3 +19,9 @@ Combine Framework
 - [[WWDC] Combine in Practice](https://developer.apple.com/videos/play/wwdc2019/721/)
 - [영상에서 보여준 문서 나중에 해봐야 할 듯하다.](https://developer.apple.com/documentation/usernotifications/registering_your_app_with_apns)
 - Network Link Conditioner
+
+### Session2 - Trackable Scroll View
+만들어 놓은 코드를 넣어 사용하는데 생각보다 이해가 안됬다..
+Swift Part1,2,3,4를 듣고나서 듣는것이 더 바람직하게 느껴진다.
+가지고 있는 내용에 비해 조금은 힘들었던 강의다.
+다시 듣고 정리하자
--- DesignCodeCombine/DesignCodeCombine/ContentView.swift
+++ DesignCodeCombine/DesignCodeCombine/ContentView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var contentOffset = CGFloat(0)
+    
     var body: some View {
         NavigationView {
             ZStack(alignment: .top) {
-                Text("Hello, world!")
-                    .padding()
+                TrackableScrollView(offsetChanged: { offsetPoint in
+                    contentOffset = offsetPoint.y
+                }) {
+                    Text("Hello, world!")
+                }
+                
+                VisualEffectBlur(blurStyle: .systemMaterial)
+                    .opacity(contentOffset < -16 ? 1 : 0)
+                    .animation(.easeIn)
+                    .ignoresSafeArea()
+                    .frame(height: 0)
             }
             .frame(maxHeight: .infinity, alignment: .top)
             .background(AccountBackground())
