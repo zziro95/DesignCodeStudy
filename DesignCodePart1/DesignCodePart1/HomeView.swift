b93e5f3b9a72529c68dfd0f3780e92759b663998
zziro95 <opers7878@gmail.com>
2021-09-14 22:29:57 +0900

    docs: part1-15 강의 듣기 및 정리

--- DesignCodePart1/DesignCodePart1/HomeView.swift
+++ DesignCodePart1/DesignCodePart1/HomeView.swift
@@ -26,9 +26,15 @@ struct HomeView: View {
             .padding(.top, 30)
 
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 30) {
+                HStack(spacing: 20) {
                     ForEach(sectionData) { item in
-                        SectionView(section: item)
+                        GeometryReader { geometry in
+                            SectionView(section: item)
+                                .rotation3DEffect(Angle(degrees:
+                                        Double(geometry.frame(in: .global).minX - 30) / -20
+                                    ), axis: (x: 0, y: 10.0, z: 0))
+                        }
+                        .frame(width: 275, height: 275)
                     }
                 }
                 .padding(30)
