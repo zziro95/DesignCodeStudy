e587fc4ee2941487c17bd06d592f7d80d6def886
Jinho Choi <jinhochoi@Jinhoui-MacBookAir.local>
2022-10-03 16:39:22 +0900

    chore: 초기 셋팅

---
+++ DesignCodePractice/DesignCodePractice/DesignCodePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodePracticeApp.swift
+//  DesignCodePractice
+//
+//  Created by Jinho Choi on 2022/10/03.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
